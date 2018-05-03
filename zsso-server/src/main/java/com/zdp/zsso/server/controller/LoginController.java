b9a7288944cc1448ff0446b44fedfa4aa75543fb
zhoudapeng <zhoudapeng@ipaynow.cn>
2018-05-03 14:58:02 +0800

    服务端登录模块优化

--- zsso-server/src/main/java/com/zdp/zsso/server/controller/LoginController.java
+++ zsso-server/src/main/java/com/zdp/zsso/server/controller/LoginController.java
@@ -5,6 +5,7 @@ import com.zdp.zsso.common.util.CookieBuilder;
 import com.zdp.zsso.server.component.ConfigLoader;
 import com.zdp.zsso.server.service.LoginService;
 import com.zdp.zsso.server.service.TokenService;
+import com.zdp.zsso.server.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -33,15 +34,37 @@ public class LoginController {
     private ConfigLoader configLoader;
     @Resource
     private TokenService tokenService;
+    @Resource
+    private UserService userService;
+
 
     @RequestMapping(value = "/login",method = RequestMethod.GET)
     public ModelAndView login(@RequestParam("systemName") String systemName,
                               @RequestParam("redirectUrl") String redirectUrl,
-                              @RequestParam(value = "errorMsg",required = false,defaultValue = "")String errorMsg) {
+                              @RequestParam(value = "errorMsg",required = false,defaultValue = "")String errorMsg,
+                              HttpServletRequest request,RedirectAttributes attrs) {
         ModelAndView mav = new ModelAndView("login/login");
-        mav.addObject("systemName",systemName);
-        mav.addObject("redirectUrl",redirectUrl);
-        mav.addObject("errorMsg",errorMsg);
+        Cookie[] cookies = request.getCookies();
+        if (cookies == null) {
+            mav.addObject("systemName",systemName);
+            mav.addObject("redirectUrl",redirectUrl);
+            mav.addObject("errorMsg",errorMsg);
+            return mav;
+        }
+        for (Cookie cookie:cookies) {
+            if (ZssoConst.COOKIE_NAME_TOKEN.equals(cookie.getName())) {
+                String token = cookie.getValue();
+                String userId = userService.query(token);
+                if (userId != null) {
+                    tokenService.bound(token,systemName);
+                    attrs.addAttribute(ZssoConst.PARAM_NAME_TOKEN,token);
+                    attrs.addAttribute(ZssoConst.PARAM_NAME_REDIRECT_URL,redirectUrl);
+                    mav.setViewName("redirect:" + configLoader.getUrl(systemName) + "/zsso/callback");
+                    return mav;
+                }
+            }
+        }
+
         return mav;
     }
 
